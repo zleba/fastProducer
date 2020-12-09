0e228cbbbf425e73f33f377f9a701cd6e9d1a07a
Patrick L.S. Connor <connorpa@mail.desy.de>
2020-12-09 17:08:48 +0100

    moving to SL7

--- installFast.sh
+++ installFast.sh
@@ -1,8 +1,6 @@
-wget -qO-  https://fastnlo.hepforge.org/code/v23/fastnlo_toolkit-2.3.1pre-2411.tar.gz | tar zxv
+wget -qO-  https://fastnlo.hepforge.org/code/v23/fastnlo_toolkit-2.3.1-2585.tar.gz | tar zxv
 mkdir -p fastnloBuild
-mv fastnlo_toolkit-2.3.1pre-2411 fastnlo
-##mv fastnlo_toolkit-2.3.1pre-2342/ fastnlo
-#mv fastnlo_toolkit-2.3.1-2585/ fastnlo
+mv fastnlo_toolkit-2.3.1-2585 fastnlo
 pwd=$PWD
 cd fastnlo
 ./configure --prefix=$pwd/fastnloBuild  --disable-doxygen-doc  --with-lhapdf=$(dirname $(dirname `which lhapdf-config`)) CXXFLAGS=" -O3 -g -fopenmp "
--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 if [ -d "/cvmfs/sft.cern.ch/lcg" ]; then
-    source /cvmfs/sft.cern.ch/lcg/views/LCG_94/x86_64-slc6-gcc7-opt/setup.sh
+    source /cvmfs/sft.cern.ch/lcg/views/LCG_94/x86_64-centos7-gcc7-opt/setup.sh
     export LHAPDF_DATA_PATH=$PWD/dpdfLib/DPDFsets:/cvmfs/sft.cern.ch/lcg/external/lhapdfsets/current/:`lhapdf-config --datadir`
 else
     echo "The cvmfs file system not mounted"
